fad6dc6849d3b1a0df7931e57f6b4775273c60bb
AndFroSwe <gummianka88@gmail.com>
2016-04-01 10:51:12 +0200

    Cleaned up code and working on encoder reading

--- Lab 1/Arduino/Lab1/Lab1.ino
+++ Lab 1/Arduino/Lab1/Lab1.ino
@@ -1,49 +1,22 @@
 #define TOP4 160  // Timer 4: 50 kHz
 #define TOP5 124  // Timer 5: 1 kHz
 
+// define globals
 float duty = 50.0;
+volatile long enc_count = 0;
 
 void setup() {
   // set output pin
   pinMode(7, OUTPUT); //OCR4B
   pinMode(46, OUTPUT); //OCR5A
+
+  initPwm(); // initiate pwm ot 50 kHz
+  initTimer(); // start 1 kHz timer
   
-  //############################
-  // ####Setup pwm####
-  // reset registers
-  TCCR4A = 0x00;
-  TCCR4B = 0x00;
-
-  // Set mode
-  TCCR4A |= (1 << WGM40) | (1 << WGM41); // phase correct mode
-  TCCR4B |= (1 << WGM43);
-  //
-  TCCR4A |= (1 << COM4B1);
-  // set clock source and prescaler
-  TCCR4B |= (1 << CS40); // internal clock source, no prescaler
-
-  // counter variables
-  OCR4A = TOP4;
-  setPWM(duty); //set dutycycle
-  //############################
-
-  //############################
-  // ####Setup velocity calculation speed####
-  // reset registers
-  TCCR5A = 0x00;
-  TCCR5B = 0x00;
-
-  // Set mode
-  TCCR5B |= (1 << WGM52); // CTC mode
-  TCCR5A |= (1 << COM5A0); // Toggle port on compare match
-
-  // set clock source and prescaler
-  TCCR5B |= (1 << CS50) | (1 << CS51); // internal clock source, prescaler 64
-
-  // counter variables
-  OCR5A = TOP5;
-  //############################
-
+  // ###### Define pin interrupts for encoder ########
+  attachInterrupt(0, encoder_isr, CHANGE);
+  attachInterrupt(1, encoder_isr, CHANGE);
+  // REMEMBER TO CHANGE THIS IN SERIAL MONITOR!
   Serial.begin(115200);
 }
 
@@ -53,8 +26,9 @@ void loop() {
   if (Serial.available() > 0) {
     duty = serialRead();
   }
-  Serial.println(duty); //Debug print
+  //Serial.println(duty); //Debug print
   setPWM(duty); //set motor PWM
+  Serial.println(enc_count);
 
 }
 
--- Lab 1/Arduino/Lab1/functions.ino
+++ Lab 1/Arduino/Lab1/functions.ino
@@ -1,21 +1,68 @@
+// initiate 50 kHz pwm
+void initPwm() {// ####Setup pwm####
+  // reset registers
+  TCCR4A = 0x00;
+  TCCR4B = 0x00;
+
+  // Set mode
+  TCCR4A |= (1 << WGM40) | (1 << WGM41); // phase correct mode
+  TCCR4B |= (1 << WGM43);
+  //
+  TCCR4A |= (1 << COM4B1);
+  // set clock source and prescaler
+  TCCR4B |= (1 << CS40); // internal clock source, no prescaler
+
+  // counter variables
+  OCR4A = TOP4;
+  setPWM(duty); //set dutycycle
+
+}
+// Set duty cycle on pwm
 void setPWM(float duty) {
   int duty_ticks;
-  
-   //Boundary value correction to become between 0-100
+
+  //Boundary value correction to become between 0-100
   if (duty > 100) {
     duty = 100;
   }
   else if (duty < 0) {
     duty = 0;
   }
-  
-  duty_ticks = int(TOP4*duty/100); // calculate number of ticks in compare register
+
+  duty_ticks = int(TOP4 * duty / 100); // calculate number of ticks in compare register
   OCR4B = duty_ticks;  //set dutycycle
 }
 
-float serialRead(void) { //Reads data from serial 0 and returns it.
+void initTimer() {
+  // reset registers
+  TCCR5A = 0x00;
+  TCCR5B = 0x00;
+
+  // Set mode
+  TCCR5B |= (1 << WGM52); // CTC mode
+  TCCR5A |= (1 << COM5A0); // Toggle port on compare match
+
+  // set clock source and prescaler
+  TCCR5B |= (1 << CS50) | (1 << CS51); // internal clock source, prescaler 64
+
+  // counter variables
+  OCR5A = TOP5;
+}
+//Reads data from serial 0 and returns it.
+float serialRead(void) {
   float inData = 0;
   inData = Serial.parseFloat();
   //Serial.println(inData); //Debug print
   return inData;
+}
+
+// read encoder and determine direction
+void encoder_isr() {
+  static int8_t lookup_table[] = {0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0};
+  static uint8_t enc_val = 0;
+
+  enc_val = enc_val << 2;
+  enc_val = enc_val | ((PINE & 0b1100) >> 2);
+
+  enc_count = enc_count + lookup_table[enc_val & 0b1111];
 }
