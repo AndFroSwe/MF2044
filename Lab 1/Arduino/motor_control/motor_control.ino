af838acc3c63dc6affd2c979e95439799f59025d
AndFroSwe <gummianka88@gmail.com>
2016-04-05 19:15:10 +0200

    continued work on feedback control
    it is unclear which variable does what in the simulink and the arduino
    script given with the task.

--- Lab 1/Arduino/motor_control/motor_control.ino
+++ Lab 1/Arduino/motor_control/motor_control.ino
@@ -2,12 +2,14 @@
 #define TOP5 248  // Timer 5: 1 kHz
 #define PPR 2000  // pulses per revolution of encoder (500 per channel)
 #define ENC_SAMPLE_RATE 1000  // rate of sampling interrupt for counting encoder, samples per second
+#define MAX_RPM 1200
+#define MIN_RPM -1200
 
 // define globals
-float duty = 50.0;
 volatile long enc_count = 0;
 long enc_count_old = 0;
 float rpm = 0;
+float duty = 50.0;
 float sample_time_ms = 100;
 
 void setup() {
@@ -25,15 +27,28 @@ void setup() {
   Serial.begin(115200);
 }
 
+float rpmToPwm(uint32_t curr_rpm) { // converts an rpm to the corresponding pwm for motor
+  // convert rpm to be within boundaries
+  if (curr_rpm > MAX_RPM) {
+    curr_rpm = MAX_RPM;
+  }
+  else if (curr_rpm < MIN_RPM) {
+    curr_rpm = MIN_RPM;
+  }
+  float pwm = 100 / (MAX_RPM - MIN_RPM) * curr_rpm + 50; // y = kx + m conversion
+  return pwm;
+}
+
+
 void loop() {
   union {
     float f1;
     uint32_t i1;
   };
-  float duty = 50.0; // Set initial duty cycle
   // read duty cycle from serial monitor
-  while (Serial.available() > 0) {  
+  while (Serial.available() > 0) {
     Serial.readBytesUntil('\r', (char *)&i1, 4);
+
     duty = f1;  // set new duty cycle
     setPWM(duty);
   }
