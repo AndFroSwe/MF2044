f8217a4c762d51de0b70964e77fcb4a5cecc68b6
AndFroSwe <gummianka88@gmail.com>
2016-04-05 17:17:01 +0200

    added own simulink controller since downloaded doesnt work

---
+++ Lab 1/Arduino/motor_control/functions.ino
@@ -0,0 +1,76 @@
+// initiate 50 kHz pwm
+void initPwm() {// ####Setup pwm####
+  // reset registers
+  TCCR4A = 0x00;
+  TCCR4B = 0x00;
+
+  // Set mode
+  TCCR4A |= (1 << WGM40) | (1 << WGM41); // phase correct mode
+  TCCR4B |= (1 << WGM43);
+  //
+  TCCR4A |= (1 << COM4B1);
+  // set clock source and prescaler
+  TCCR4B |= (1 << CS40); // internal clock source, no prescaler
+
+  // counter variables
+  OCR4A = TOP4;
+  setPWM(duty); //set dutycycle
+}
+
+// Set duty cycle on pwm
+void setPWM(float duty) {
+  int duty_ticks;
+
+  //Boundary value correction to become between 0-100
+  if (duty > 100) {
+    duty = 100;
+  }
+  else if (duty < 0) {
+    duty = 0;
+  }
+
+  duty_ticks = int(TOP4 * duty / 100); // calculate number of ticks in compare register
+  OCR4B = duty_ticks;  //set dutycycle
+}
+
+//Initiate 1 kHz timer
+void initTimer() {
+  // reset registers
+  TCCR5A = 0x00;
+  TCCR5B = 0x00;
+  // Set mode
+  TCCR5B |= (1 << WGM52); // CTC mode
+  // set clock source and prescaler
+  TCCR5B |= (1 << CS50) | (1 << CS51); // internal clock source, prescaler 64
+  // timer mask register set
+  TIMSK5 |= (1 << OCIE5A);
+  // counter variables
+  OCR5A = TOP5*sample_time_ms;
+}
+
+//Reads data from serial 0 and returns it.
+float serialRead(void) {
+  float inData = 0;
+  inData = Serial.parseFloat();
+  //Serial.println(inData); //Debug print
+  return inData;
+}
+
+// read encoder and determine direction
+void encoder_isr() {
+  static int8_t lookup_table[] = {0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0};
+  static uint8_t enc_val = 0;
+
+  enc_val = enc_val << 2;    // make room for new readings
+  enc_val = enc_val | ((PINE & 0b110000) >> 4);  // mask in pin 4 and 5 in register pine
+
+  enc_count = enc_count + lookup_table[enc_val & 0b1111];  // update encoder history
+}
+
+// timer interrupt for calculating speed on motor.
+ISR(TIMER5_COMPA_vect) {  // interrupt on 1 kHz clock
+  // THIS NEED S ADJUSTING
+  rpm = float(enc_count)*60*ENC_SAMPLE_RATE/sample_time_ms/PPR; // calculate rotational speed with conversion factor 60*samplerate/ppr
+  enc_count = 0;  // reset encoder counter to counter overflow
+}
+
---
+++ Lab 1/Arduino/motor_control/motor_control.ino
@@ -0,0 +1,44 @@
+#define TOP4 160  // Timer 4: 50 kHz
+#define TOP5 248  // Timer 5: 1 kHz
+#define PPR 2000  // pulses per revolution of encoder (500 per channel)
+#define ENC_SAMPLE_RATE 1000  // rate of sampling interrupt for counting encoder, samples per second
+
+// define globals
+float duty = 50.0;
+volatile long enc_count = 0;
+long enc_count_old = 0;
+float rpm = 0;
+float sample_time_ms = 100;
+
+void setup() {
+  // set output pin
+  pinMode(7, OUTPUT); //OCR4B
+
+  initPwm(); // initiate pwm ot 50 kHz
+  initTimer(); // start 1 kHz timer
+
+  // ###### Define pin interrupts for encoder ########
+  attachInterrupt(0, encoder_isr, CHANGE);
+  attachInterrupt(1, encoder_isr, CHANGE);
+
+  // REMEMBER TO CHANGE THIS IN SERIAL MONITOR!
+  Serial.begin(115200);
+}
+
+void loop() {
+  union {
+    float f1;
+    uint32_t i1;
+  };
+  float duty = 50.0; // Set initial duty cycle
+  // read duty cycle from serial monitor
+  while (Serial.available() > 0) {  
+    Serial.readBytesUntil('\r', (char *)&i1, 4);
+    duty = f1;  // set new duty cycle
+    setPWM(duty);
+  }
+  Serial.write((const char*)&rpm, 4);
+  delay(100);
+}
+
+
