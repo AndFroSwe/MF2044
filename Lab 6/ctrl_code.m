67f8ae6a5a1b83b665cf709f2a838d61c16c6cb1
AndFroSwe <gummianka88@gmail.com>
2016-05-11 09:38:38 +0200

    Messaging works

--- Lab 6/ctrl_code.m
+++ Lab 6/ctrl_code.m
@@ -2,31 +2,28 @@ function [ exectime, data ] = ctrl_code( segment, data )
 switch segment
     case 1
         data.msgId = 3;
-        msg = ['r', 'y'];
-        while ~(isempty(msg)) %Empty queue
-            msg = ttGetMsg;
-           
-            if isempty(msg) %Exit if empty
-                exectime = -1;
-            else
+%         msg = ['r', 'y'];
+        msg = ttGetMsg;
+        if isempty(msg) %Exit if empty
+            exectime = -1;
+        else
+            while ~(isempty(msg)) %Empty queue            
                 disp(msg(1))
-                
                 if (msg(1) == 1) % read r
                     disp('r')
                     data.r = msg(2);
                     %fprintf('Ctrl_code recieved r: %f \n', data.r)
-                
                 elseif (msg(1) == 2) % read y
                     disp('y')
                     data.y = msg(2);
                     fprintf('Ctrl_code recieved y: %f \n', data.y)
                 end       
-                data = pidcalc(data, data.r, data.y);
-                exectime = data.wcet;
+                msg = ttGetMsg;
             end
+            data = pidcalc(data, data.r, data.y);
+            exectime = data.wcet;
         end
-                    
-        
+
     case 2
         ttAnalogOut(data.uChan, data.u);
         ttSendMsg(2, [data.msgId, data.u], 40);
