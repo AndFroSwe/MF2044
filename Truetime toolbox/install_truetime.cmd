5e81a506103d8d83c42a9f8eb659445f01321ef2
Peter Fjellander <peterfj@kth.se>
2016-06-02 15:38:29 +0200

    Changed script to work without matlab started.

--- Truetime toolbox/install_truetime.cmd
+++ Truetime toolbox/install_truetime.cmd
@@ -6,7 +6,7 @@ SET tar_folder=C:\Users\Public\Documents\MATLAB\_old
 SET "curpath=%cd%"
 SET "truetime=%curpath%\truetime-2.0
 
-
+mkdir "C:%homepath%\Documents\MATLAB\"
 COPY "%curpath%\init_truetime.m" "C:%homepath%\Documents\MATLAB\startup.m"
 
 RD /S /Q %tar_folder%
